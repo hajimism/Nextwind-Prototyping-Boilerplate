f3ccbb29edea8b5733291d321b25df2d1b4790c7
Hajime Nakagawa <navyblue0914@icloud.com>
2022-01-28 18:46:57 +0900

    test husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,8 @@ const Home: NextPage = () => {
 
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
   );
