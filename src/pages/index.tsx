80cef2e588587575b5075cc51c3c361fd60b90db
Hajime Nakagawa <navyblue0914@icloud.com>
2022-01-28 19:43:35 +0900

    setup eslint and test husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,6 @@ const Home: NextPage = () => {
 
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
   );
