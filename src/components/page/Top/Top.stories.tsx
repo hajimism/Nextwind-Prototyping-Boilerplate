4a8d759f878c6a0eab0a91239fabdc88e5e8fe0e
Hajime Nakagawa <navyblue0914@icloud.com>
2022-02-01 12:27:09 +0900

    update sb

--- src/components/page/Top/Top.stories.tsx
+++ src/components/page/Top/Top.stories.tsx
@@ -10,8 +10,29 @@ export default {
   },
 } as ComponentMeta<typeof TopPageView>;
 
-const Template: ComponentStory<typeof TopPageView> = (args) => (
-  <TopPageView {...args} />
+const Small: ComponentStory<typeof TopPageView> = (args) => (
+  <div className="max-w-sm">
+    <TopPageView {...args} />
+  </div>
 );
 
-export const Default = Template.bind({});
+const Medium: ComponentStory<typeof TopPageView> = (args) => (
+  <div className="max-w-md">
+    <TopPageView {...args} />
+  </div>
+);
+
+const Large: ComponentStory<typeof TopPageView> = (args) => (
+  <div className="max-w-6xl">
+    <TopPageView {...args} />
+  </div>
+);
+
+export const Mobile = Small.bind({});
+Mobile.args = {};
+
+export const Tablet = Medium.bind({});
+Tablet.args = {};
+
+export const Desktop = Large.bind({});
+Desktop.args = {};
