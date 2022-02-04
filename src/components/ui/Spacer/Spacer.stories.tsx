63c1e746dfbe715c7e9b4c9731e4531e71ceb79d
Hajime Nakagawa <navyblue0914@icloud.com>
2022-02-04 16:22:28 +0900

    update Spacer

---
+++ src/components/ui/Spacer/Spacer.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+
+import { Spacer } from "./index";
+
+export default {
+  title: "UI/Spacer",
+  component: Spacer,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Spacer>;
+
+const Template: ComponentStory<typeof Spacer> = (args) => <Spacer {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 36,
+};
