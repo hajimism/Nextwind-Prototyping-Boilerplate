0e89777c05210c568e4e53b183dca63dfc2d1a29
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-07 08:57:40 +0900

    remove msw as it's too much for this template

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,8 @@
 import "../src/styles/globals.css"
 import * as NextImage from "next/image"
-import { initialize, mswDecorator } from "msw-storybook-addon"
-import { rest } from "msw"
 
 import { withConsole } from "@storybook/addon-console"
 import { addDecorator } from "@storybook/react"
-
-// Initialize MSW
-initialize()
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context))
 
@@ -29,18 +24,4 @@ export const parameters = {
   previewTabs: {
     "storybook/docs/panel": { index: -1 },
   },
-  msw: {
-    handlers: [
-      // example
-      rest.get("/hello", (req, res, ctx) => {
-        return res(
-          ctx.json({
-            name: "John Doe",
-          })
-        )
-      }),
-    ],
-  },
 }
-
-export const decorators = [mswDecorator]
